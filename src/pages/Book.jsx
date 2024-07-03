39235a8b1925ae78b49055cc6af3ce54f6822988
Codeuzz <belinda.zilevu@gmail.com>
2024-07-03 19:17:16 +0200

    removed Routes test

--- src/pages/Book.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom"
-
-function Book() {
-    const {id} = useParams()
-    return <h1>Book {id}</h1>
-}
-
-export default Book
--- src/pages/NewBook.jsx
+++
@@ -1,5 +0,0 @@
-function NewBook() {
-    return <h1>NewBook</h1>
-}
-
-export default NewBook
